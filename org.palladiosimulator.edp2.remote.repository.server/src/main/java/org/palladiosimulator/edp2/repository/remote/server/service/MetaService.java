807e030d8e778ce14b62e91fdff39f5cb22f86a4
Alexander Lüngen <alex.luengen@melodyscanner.com>
2020-02-29 18:16:20 +0100

    Implementation, documentation and  tests for MetaSerivce.

--- org.palladiosimulator.edp2.remote.repository.server/src/main/java/org/palladiosimulator/edp2/repository/remote/server/service/MetaService.java
+++ org.palladiosimulator.edp2.remote.repository.server/src/main/java/org/palladiosimulator/edp2/repository/remote/server/service/MetaService.java
@@ -1,29 +1,75 @@
 package org.palladiosimulator.edp2.repository.remote.server.service;
 
+import javax.measure.Measure;
+import javax.measure.unit.SI;
+
+import org.eclipse.emf.common.util.EList;
 import org.palladiosimulator.edp2.models.ExperimentData.ExperimentDataFactory;
 import org.palladiosimulator.edp2.models.ExperimentData.ExperimentGroup;
+import org.palladiosimulator.edp2.models.ExperimentData.ExperimentRun;
+import org.palladiosimulator.edp2.models.ExperimentData.ExperimentSetting;
+import org.palladiosimulator.edp2.models.ExperimentData.MeasuringType;
 import org.palladiosimulator.edp2.models.Repository.Repository;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPoint;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPointRepository;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringpointFactory;
+import org.palladiosimulator.edp2.models.measuringpoint.StringMeasuringPoint;
+import org.palladiosimulator.edp2.remote.dto.ExperimentRunDTO;
+import org.palladiosimulator.edp2.remote.dto.ExperimentSettingDTO;
+import org.palladiosimulator.edp2.remote.dto.MeasuringPointDTO;
+import org.palladiosimulator.edp2.remote.dto.MeasuringTypeDTO;
 import org.palladiosimulator.edp2.repository.remote.server.util.UUIDConverter;
+import org.palladiosimulator.metricspec.MetricDescription;
+import org.palladiosimulator.metricspec.constants.MetricDescriptionConstants;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MetaService {
 
 	private final static ExperimentDataFactory EXPERIMENT_DATA_FACTORY = ExperimentDataFactory.eINSTANCE;
-
+	private final static MeasuringpointFactory MEASURING_POINT_FACTORY = MeasuringpointFactory.eINSTANCE;
 	
+	
+	/**
+	 * Creates a new ExperimentGroup inside the referenced Repository with 
+	 * the groupName as the Purpose of the ExperimentGroup.
+	 * In addition a MeasuringPointRepository is created in the ExperimentGroup.
+	 * @param repo 
+	 * 			Repository where the ExperimentGroup is added to
+	 * @param groupName 
+	 * 			Set as purpose of the ExperimentGroup
+	 * @return Reference to the new created ExperimentGroup
+	 */
 	public ExperimentGroup createExperimentGroup(Repository repo, String groupName) {
 		ExperimentGroup expGrp = EXPERIMENT_DATA_FACTORY.createExperimentGroup();
 		
 		expGrp.setPurpose(groupName);
 		expGrp.setRepository(repo);
 		
+		MeasuringPointRepository mpRepo = MEASURING_POINT_FACTORY.createMeasuringPointRepository();
+		
+		if(expGrp.getMeasuringPointRepositories() != null 
+				&& expGrp.getMeasuringPointRepositories().isEmpty()) {
+			expGrp.getMeasuringPointRepositories().add(mpRepo);
+		}
+		
 		return expGrp;
 	}
 	
-	public ExperimentGroup getExperimentGroupFromRepository(Repository repo, String expGrpId) {
-		
-		String internalId = UUIDConverter.getBase64FromUuid(expGrpId);
+	/**
+	 * Returns the ExperimentGroup related with the API-UUID within the referenced Repository.
+	 * Returns null if there is no ExperimentGroup found in the Repository, or the API-UUID is
+	 * null or invalid.
+	 * @param repo 
+	 * 			Repository where ExperimentGroup is searched.
+	 * @param expGrpUuid 
+	 * 			API-UUID related to the ExperimentGroup
+	 * @return Reference to the ExperimentGroup or null, if non is found or the API-UUID is invalid.
+	 */
+	public ExperimentGroup getExperimentGroupFromRepository(Repository repo, String expGrpUuid) {
+		if(expGrpUuid == null || expGrpUuid.isEmpty())
+			return null;
+		String internalId = UUIDConverter.getBase64FromUuid(expGrpUuid);
 		
 		return repo.getExperimentGroups()
 				.stream()
@@ -31,4 +77,162 @@ public class MetaService {
 				.findAny()
 				.orElse(null);
 	}
+	
+	
+	/**
+	 * Creates a new ExperimentSetting, as specified in the settingDTO. The new ExperimentSetting is added
+	 * to the references Repository and the ExperimentGroup, as specified by the related API-UUID of the
+	 * ExperimentGroup.
+	 * @param repo
+	 * 			Reference to the Repository, the ExperimentSetting should be added to.
+	 * @param expGrpUuid
+	 * 			API-UUID of the ExperimentGroup, the ExperimentSetting should be added to.
+	 * @param settingDTO
+	 * 			Model describing the ExperimentSetting that should be created.
+	 * @return Reference to the newly created ExperimentSetting, or null if the Model is missing,
+	 * 			does not contain a description, the ExperimentGroup does not exist within the 
+	 * 			Repository.
+	 */
+	public ExperimentSetting createExperimentSetting(Repository repo, String expGrpUuid, ExperimentSettingDTO settingDTO) {
+		
+		if(settingDTO == null || settingDTO.getDescription() == null)
+			return null;
+		
+		ExperimentGroup grp = getExperimentGroupFromRepository(repo, expGrpUuid);
+		
+		if(grp != null) {
+			ExperimentSetting setting = EXPERIMENT_DATA_FACTORY.createExperimentSetting(grp, settingDTO.getDescription());
+			
+			return setting;
+		}
+		return null;
+	}
+
+	/**
+	 * Returns the ExperimentSetting specified by the API-UUID that should exist within the 
+	 * ExperimentGroup. 
+	 * @param expGrp 
+	 * 			ExperimentGroup that should contain the ExperimentSettings.
+	 * @param settingUuid
+	 * 			API-UUID related to the ExperimentSetting, that should be returned.
+	 * @return
+	 * 			ExperimentSetting related to the API-UUID or null, if non is found.
+	 */
+	public ExperimentSetting getExperimentSettingFromExperimentGroup(ExperimentGroup expGrp, String settingUuid) {
+		String internalId = UUIDConverter.getBase64FromUuid(settingUuid);
+		
+		ExperimentSetting setting = expGrp.getExperimentSettings()
+				.stream()
+				.filter(set -> set.getId().matches(internalId))
+				.findAny()
+				.orElse(null);
+		
+		return setting;
+	}
+	
+	/**
+	 * Creates a new ExperimentRun within the referenced Repository and within the ExperimentGroup and ExperimentSetting
+	 * as specified by their related API-UUIDs. The ExperimentRun is created as described by the ExperimentRunDTO.
+	 * @param repo
+	 * 			Repository where the ExperimentRun is added to.
+	 * @param expGrpUuid
+	 * 			API-UUID related to the ExperimentGroup where the ExperimentRun is added to.
+	 * @param settingUuid
+	 * 			API-UUID related to the ExperimentSetting where the ExperimentRun is added to.
+	 * @param runDTO
+	 * 			Model describing the ExperimentRun.
+	 * @return 
+	 * 			Reference to created ExperimentRun or null, if there is no ExperimentGroup or ExperimentSetting
+	 * 			related to their respective API-UUID.
+	 */
+	public ExperimentRun createExperimentRun(Repository repo, String expGrpUuid, String settingUuid, ExperimentRunDTO runDTO) {
+		ExperimentGroup expGrp = getExperimentGroupFromRepository(repo, expGrpUuid);
+		if(expGrp != null) {
+			ExperimentSetting setting = getExperimentSettingFromExperimentGroup(expGrp, settingUuid);
+			if(setting != null) {
+				ExperimentRun run = EXPERIMENT_DATA_FACTORY.createExperimentRun(setting);	
+				run.setStartTime(runDTO.getStartTime());
+				run.setDuration(Measure.valueOf(runDTO.getDuration(), SI.SECOND));
+				return run;
+			}
+		}		
+		return null;
+	}
+	
+	/**
+	 * Returns the ExperimentRun related to the given API-UUID that should be contained in the ExperimentSetting.
+	 * @param setting
+	 * 			ExperimentSetting that should contain the ExperimentRun.
+	 * @param runUuid
+	 * 			API-UUID related to the ExperimentRun
+	 * @return
+	 * 			Reference to the ExperimentRun or null, if there is no ExperimentRun related the the API-UUID
+	 * 			in the given ExperimentSetting.
+	 */
+	public ExperimentRun getExperimentRunFromExperimentSetting(ExperimentSetting setting, String runUuid) {
+		String internalId = UUIDConverter.getUuidFromBase64(runUuid);
+		
+		ExperimentRun run = setting.getExperimentRuns()
+				.stream()
+				.filter(r -> r.getId().matches(internalId))
+				.findAny()
+				.orElse(null);
+		
+		return run;
+	}
+	
+	/**
+	 * Creates a new StringMeasuringPoint within the ExperimentGroup as modeled by the MeasuringPointDTO.
+	 * @param grp
+	 * 			ExperimentGroup the StringMeasuringPoint should be created in.
+	 * @param dto
+	 * 			Model specifying the StringMeasruingPoint to create.
+	 * @return
+	 * 			Reference to the new created StringMeasuringPoint instance or null, if the ExperimentGroup
+	 *			or the MeasuringPointDTO is null.
+	 */
+	public StringMeasuringPoint createStringMeasuringPoint(ExperimentGroup grp, MeasuringPointDTO dto) {
+		if(grp == null || dto == null) {
+			return null;
+		}
+		
+		StringMeasuringPoint smp = MEASURING_POINT_FACTORY.createStringMeasuringPoint();
+		
+		if( smp != null) {
+			smp.setMeasuringPoint(dto.getMeasuringPointDescription());
+			
+			// only one MP-Repo per Group
+			MeasuringPointRepository mpRepo = grp.getMeasuringPointRepositories().get(0);
+			
+			if(mpRepo == null) {
+				mpRepo = MEASURING_POINT_FACTORY.createMeasuringPointRepository();
+				grp.getMeasuringPointRepositories().add(mpRepo);
+			}
+			smp.setMeasuringPointRepository(mpRepo);
+		}
+		return null;
+	}
+
+	public MeasuringType createMeasuringType(ExperimentGroup expGrp, MeasuringTypeDTO mtDTO) {
+		
+		// 1. get measuring point from MeasuringPointRepository
+		MeasuringPoint mPoint = null;
+		for(MeasuringPoint point: getMeasuringPointsFromExperimentGroup(expGrp)) {
+			if(point.getStringRepresentation().equals(mtDTO.getMeasuringPointStringRepresentation())) {
+				mPoint = point;
+				break;
+			}
+		}
+		MetricDescription description = MetricDescriptionConstants.AGGREGATED_COST_OVER_TIME;
+		
+		// 2. Create Metric Description
+		
+		MeasuringType mType = null;
+		return null;
+	}
+	
+	public EList<MeasuringPoint> getMeasuringPointsFromExperimentGroup(ExperimentGroup grp) {
+		return grp.getMeasuringPointRepositories().get(0).getMeasuringPoints();
+	}	
+	
 }
--- org.palladiosimulator.edp2.remote.repository.server/src/test/java/org/palladiosimulator/edp2/remote/repository/service/TestMetaService.java
+++ org.palladiosimulator.edp2.remote.repository.server/src/test/java/org/palladiosimulator/edp2/remote/repository/service/TestMetaService.java
@@ -2,20 +2,33 @@ package org.palladiosimulator.edp2.remote.repository.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.palladiosimulator.edp2.models.ExperimentData.ExperimentDataFactory;
 import org.palladiosimulator.edp2.models.ExperimentData.ExperimentGroup;
+import org.palladiosimulator.edp2.models.ExperimentData.ExperimentRun;
+import org.palladiosimulator.edp2.models.ExperimentData.ExperimentSetting;
 import org.palladiosimulator.edp2.models.Repository.Repository;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringPointRepository;
+import org.palladiosimulator.edp2.models.measuringpoint.MeasuringpointFactory;
+import org.palladiosimulator.edp2.models.measuringpoint.StringMeasuringPoint;
+import org.palladiosimulator.edp2.remote.dto.ExperimentRunDTO;
+import org.palladiosimulator.edp2.remote.dto.ExperimentSettingDTO;
+import org.palladiosimulator.edp2.remote.dto.MeasuringPointDTO;
 import org.palladiosimulator.edp2.repository.remote.server.service.MetaService;
 import org.palladiosimulator.edp2.repository.remote.server.util.UUIDConverter;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
 public class TestMetaService extends BasicEDP2Test {
 
 	static MetaService mService = new MetaService();
 	
 	@Test
-	void createNewExperimentGroup_shouldBeContainedInRepository() {
+	void createNewExperimentGroup_shouldBeContainedInRepositoryandContainsPurposeAndMeasuringPointRepository() {
 		Repository ldRepo = repoService.createNewRepository();
 		
 		String testPurpose = "test";
@@ -23,6 +36,8 @@ public class TestMetaService extends BasicEDP2Test {
 		ExperimentGroup grp = mService.createExperimentGroup(ldRepo, testPurpose);
 		
 		assertThat("New ExperimentGroup should be in Repository", ldRepo.getExperimentGroups(), hasItem(grp));
+		Assertions.assertNotNull(grp.getMeasuringPointRepositories().get(0), "Contains a MeasuringPointRepository.");
+		Assertions.assertTrue(grp.getMeasuringPointRepositories().size() == 1, "Contains exactly one MeasuringPointRepository.");
 		Assertions.assertEquals(testPurpose, grp.getPurpose());
 	}
 	
@@ -38,4 +53,157 @@ public class TestMetaService extends BasicEDP2Test {
 		
 		Assertions.assertEquals(grp, testGrp);
 	}
+	
+	@Test
+	void createExperimentSetting_shouldReturnExperimentSettingContainedInExperimentGroup() {
+			
+		Repository repo = repoService.createNewRepository();
+		ExperimentGroup expGrp = ExperimentDataFactory.eINSTANCE.createExperimentGroup();
+		repo.getExperimentGroups().add(expGrp);
+		
+		for(int i = 0; i < 10; i++) {
+			ExperimentGroup temp = ExperimentDataFactory.eINSTANCE.createExperimentGroup();
+			repo.getExperimentGroups().add(temp);
+		}
+		
+		String groupUuid = UUIDConverter.getUuidFromBase64(expGrp.getId());
+		
+		ExperimentSettingDTO settingDTO = new ExperimentSettingDTO();
+		settingDTO.setGroupId(groupUuid);
+		settingDTO.setDescription("Test");
+		
+		
+		ExperimentSetting testSetting = mService.createExperimentSetting(repo, groupUuid, settingDTO);
+		
+		
+		Assertions.assertNotNull(testSetting);
+		assertThat("ExperimentGroup should contain ExperimentSetting.", 
+				expGrp.getExperimentSettings(), 
+				hasItem(testSetting));
+		Assertions.assertEquals(
+				settingDTO.getDescription(), 
+				testSetting.getDescription(), 
+				"But Description was: " + testSetting.getDescription());
+	}
+	
+	@Test
+	void providingWrongIdsOrEmptyRepo_createExperimentSettingShouldReturnNull() {
+		
+		Repository testRepo = repoService.createNewRepository();
+		
+		ExperimentSetting testSetting1 = mService.createExperimentSetting(null, null, null);
+		ExperimentSetting testSetting2 = mService.createExperimentSetting(testRepo, null, null);
+		
+		ExperimentGroup testGroup = ExperimentDataFactory.eINSTANCE.createExperimentGroup();
+		testRepo.getExperimentGroups().add(testGroup);
+		String groupId = UUIDConverter.getUuidFromBase64(testGroup.getId());
+		ExperimentSetting testSetting3 = mService.createExperimentSetting(
+				testRepo, 
+				groupId, 
+				null);
+		
+		ExperimentSettingDTO dto = new ExperimentSettingDTO();
+		ExperimentSetting testSetting4 = mService.createExperimentSetting(testRepo, groupId, dto);
+		
+		String invalidUuid = UUID.randomUUID().toString();
+		dto.setDescription("Description");
+		ExperimentSetting testSetting5 = mService.createExperimentSetting(testRepo, invalidUuid, dto);
+		
+		Assertions.assertNull(testSetting1, "All parameters are null, should return null");
+		Assertions.assertNull(testSetting2, "Empty Repository with null parameters should return null");
+		Assertions.assertNull(testSetting3, "Repository with Group and valid groupId, SettingDTO is null");
+		Assertions.assertNull(testSetting4, "Repository with Group and valid groupId, empty SettingsDTO");
+		Assertions.assertNull(testSetting5, "Repository with Group and valid SettingsDTO, invalid GroupId");
+	}
+	
+	@Test 
+	void providingValidInput_getExperimentSettingFromExperimentGroup_ShouldReturn_ExperimentSetting() {
+		
+		ExperimentGroup grp = ExperimentDataFactory.eINSTANCE.createExperimentGroup();
+		ExperimentSetting setting = ExperimentDataFactory.eINSTANCE.createExperimentSetting(grp, "Test");
+		grp.getExperimentSettings().add(setting);
+		String validSettingId = UUIDConverter.getUuidFromBase64(setting.getId());
+		
+		// create some random Settings
+		int settingsCount = 10;
+		for(int i = 0; i < settingsCount; i++) {
+			ExperimentSetting temp = ExperimentDataFactory.eINSTANCE.createExperimentSetting(grp, "Test");
+			grp.getExperimentSettings().add(temp);
+		}
+		
+		ExperimentSetting testSetting = mService.getExperimentSettingFromExperimentGroup(grp, validSettingId);
+		
+		Assertions.assertNotNull(testSetting, "Returned ExperimentSetting is not null");
+		Assertions.assertEquals(setting, testSetting, "Should be the same object");
+		Assertions.assertEquals(setting.getId(), testSetting.getId(), "Ids should be the same");
+		Assertions.assertEquals(setting.getDescription(), testSetting.getDescription(), "Description should be the same");
+	}
+	
+	@Test
+	void providingValidData_createExperimentRun_ShouldReturn_ExperimentRun() {
+		// arrange
+		Repository repo = repoService.createNewRepository();
+		
+		ExperimentGroup grp = ExperimentDataFactory.eINSTANCE.createExperimentGroup();
+		grp.setRepository(repo);
+		
+		
+		ExperimentSetting setting = ExperimentDataFactory.eINSTANCE.createExperimentSetting();
+		setting.setExperimentGroup(grp);
+		
+		String grpApiUuid = UUIDConverter.getUuidFromBase64(grp.getId());
+		String setApiUuid = UUIDConverter.getUuidFromBase64(setting.getId());
+		
+		ExperimentRunDTO dto = new ExperimentRunDTO();
+		dto.setDuration(1000000000);
+		
+		Date startTime = Date.from(Instant.now());
+		dto.setStartTime(startTime);
+		
+		// act
+		ExperimentRun testRun = mService.createExperimentRun(repo, grpApiUuid, setApiUuid, dto);
+		
+		// assert
+		Assertions.assertNotNull(testRun, "Testrun should not be null");
+		Assertions.assertEquals(setting, testRun.getExperimentSetting(), "ExperimentSetting should be set to the provided.");
+		Assertions.assertEquals(grp, testRun.getExperimentSetting().getExperimentGroup(), "ExperimentGroup should");
+		assertThat("ExperimentRun should be contained in ExperimentGroups-ExperimentRuns.", 
+				testRun.getExperimentSetting().getExperimentRuns(), 
+				hasItem(testRun));
+	}
+	
+	void providingValidData_createStringMeasuringPoint_ShouldReturn_StringMeasuringPoint() {
+		ExperimentGroup grp = ExperimentDataFactory.eINSTANCE.createExperimentGroup();
+		
+		MeasuringPointRepository mpRepo = MeasuringpointFactory.eINSTANCE.createMeasuringPointRepository();
+		grp.getMeasuringPointRepositories().add(mpRepo);
+		
+		String description = "Testpunk 1";
+		MeasuringPointDTO dto = new MeasuringPointDTO();
+		dto.setMeasuringPointDescription(description);
+		
+		StringMeasuringPoint testSmp = mService.createStringMeasuringPoint(grp, dto);
+		
+		Assertions.assertNotNull(testSmp, "StringMeasuringPoint should not be null.");
+		Assertions.assertEquals(description, testSmp.getMeasuringPoint());
+		Assertions.assertEquals(grp.getMeasuringPointRepositories().get(0), 
+				testSmp.getMeasuringPointRepository(), 
+				"MeasuringPoint should reference same MeasuringPointRepository as the ExperimentGroup.");
+		assertThat("StringMeasuringPoint should be contained in MeasuringPointRepository of the ExperimentGroup.",
+				grp.getMeasuringPointRepositories().get(0).getMeasuringPoints(), hasItem(testSmp));
+
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
