f3285fe6edd0a56b463b99c8a55021c049bf8ccf
Alexander Lüngen <alex.luengen@melodyscanner.com>
2020-03-04 15:54:14 +0100

    Added List of MeasuringTypes to the  ExperimentSetting Model.

--- org.palladiosimulator.edp2.remote.repository.api/src/main/java/org/palladiosimulator/edp2/remote/dto/ExperimentSettingDTO.java
+++ org.palladiosimulator.edp2.remote.repository.api/src/main/java/org/palladiosimulator/edp2/remote/dto/ExperimentSettingDTO.java
@@ -11,6 +11,8 @@ public class ExperimentSettingDTO {
 	private String groupId;
 	
 	private List<String> experimentRunIds;
+	
+	private List<String> measuringTypeIds;
 
 	public String getDescription() {
 		return description;
@@ -43,7 +45,12 @@ public class ExperimentSettingDTO {
 	public void setExperimentRunIds(List<String> experimentRunIds) {
 		this.experimentRunIds = experimentRunIds;
 	}
-	
-	
-	
+
+	public List<String> getMeasuringTypeIds() {
+		return measuringTypeIds;
+	}
+
+	public void setMeasuringTypeIds(List<String> measuringTypeIds) {
+		this.measuringTypeIds = measuringTypeIds;
+	}	
 }
